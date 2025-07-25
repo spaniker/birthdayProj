30e7bcf6c4564e899e037f456cd7cd182226621f
spaniker <84048769+spaniker@users.noreply.github.com>
2025-07-25 09:58:26 -0700

    Initial Commit

---
+++ birthdayProj/birthdayProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  birthdayProjApp.swift
+//  birthdayProj
+//
+//  Created by Syona Paniker on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct birthdayProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
